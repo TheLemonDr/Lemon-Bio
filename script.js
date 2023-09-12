c391c74d5fd6c4f7e5436add916a4575f56084b7
Dr. Lemon <120197740+TheLemonDr@users.noreply.github.com>
2023-09-12 00:11:32 -0400

    ???????

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function gone() {
-  var bioContainer = document.querySelector('.bio-container');
-  var bioContainer2 = document.querySelector('.bio-container2');
-
-  bioContainer.style.display = 'none';
-  bioContainer2.style.display = 'block';
-}
-
-document.querySelector('.bio-container a').addEventListener('click', gone);
+    var bioContainer = document.querySelector('.bio-container');
+    var bioContainer2 = document.querySelector('.bio-container2');
+  
+    bioContainer.style.display = 'none';
+    bioContainer2.style.display = 'block';
+  }
+  
+  document.querySelector('.bio-container a').addEventListener('click', gone);
+  
