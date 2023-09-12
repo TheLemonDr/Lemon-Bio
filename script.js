b30af79e7ff93a341c7c9ee734bbead8f16c6b52
Dr. Lemon <120197740+TheLemonDr@users.noreply.github.com>
2023-09-11 23:56:06 -0400

    lemon bio

---
+++ script.js
@@ -0,0 +1,9 @@
+function gone() {
+  var bioContainer = document.querySelector('.bio-container');
+  var bioContainer2 = document.querySelector('.bio-container2');
+
+  bioContainer.style.display = 'none';
+  bioContainer2.style.display = 'block';
+}
+
+document.querySelector('.bio-container a').addEventListener('click', gone);
